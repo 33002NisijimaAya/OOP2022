34ccc28617c76c1b142e5f086db366bd243473f8
33002西島彩 <33002@ojs.ac.jp>
2022-05-10 15:54:30 +0900

    問題2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,7 +7,27 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+#if false 
+            List<Song> songs = new List<Song>();
+            var song1 = new Song("アナと雪の女王", "アナ", 60);
+            songs.Add(song1);
+            var song2 = new Song("女々しくて", "ゴールデンボンバー", 60);
+            songs.Add(song2);
+            var song3 = new Song("アンパンマン", "食パンまん", 60);
+            songs.Add(song3);
+#endif
+            var songs = new Song[] {
+                new Song("女々しくて", "ゴールデンボンバー", 522),
+                new Song("アナと雪の女王", "アナ", 312),
+                new Song("アンパンマン", "食パンまん", 60),
+            };
 
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+           
         }
     }
 }
--- Chapter02/Exercise03/Song.cs
+++ Chapter02/Exercise03/Song.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Exercise03 {
     class Song {
-        public string Title { get; set; }//歌のタイトル
-        public string AeristName { get; set; }//アーティスト名
-        public int Length { get; set; }//演奏時間（秒）
-
-        public Song(string Title, string ArtistName, int Length) {
-
-        }
+     
     }
 }
