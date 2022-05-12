6b871c79b1a2203201861388156414ba6e541723
33002西島彩 <33002@ojs.ac.jp>
2022-05-12 14:45:43 +0900

    問題2.2完成

--- Chapter02/Exercise02/InchConverter.cs
+++ Chapter02/Exercise02/InchConverter.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class InchConverter {
         private static readonly double ratio = 0.0254;//←外部からもアクセスさせたい場合
+        //メートルからインチを求める（静的メソッド）
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        
         //インチからメートルを求める（静的メソッド）
-        public static double FromMeter(double inch) {
+        public static double ToMeter(double inch) {
             return inch * ratio;
         }
     }
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace Exercise02 {
     class Program {
+        //コマンドライン引数
         static void Main(string[] args) {
-            PrintInchToMeterList(1, 10);
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //インチからメートルへの対応表を出力
+                PrintFeetToMeterList(1, 10);
+            } else if (args.Length >= 1 && args[0] == "-toi") {
+                //メートルからインチへの対応表を出力
+                PrintMeterToFeetList(1, 10);
+            }
         }
 
         //インチからメートルへの対応表を出力
-        private static void PrintInchToMeterList(int start, int stop) {
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintFeetToMeterList(int start, int stop) {
             for (int meter = start; meter <= stop; meter++) {
                 double inch = InchConverter.FromMeter(meter);
-                Console.WriteLine("{0} inch = {1:0.0000} m", meter, inch);
+                Console.WriteLine("{0} m = {1:0.0000}inch ", meter, inch);
             }
         }
     }
