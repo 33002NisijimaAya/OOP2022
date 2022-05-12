e45b2e7c3c8eef55fde0593bf39d34f747151c65
33002西島彩 <33002@ojs.ac.jp>
2022-05-12 15:08:54 +0900

    問題2.3完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -7,7 +7,20 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-
+            var sales = new SalesCounter("sales.csv");
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1.店舗別");
+            Console.WriteLine("2.商品カテゴリー別");
+            IDictionary<string, int> amountPerStore;
+            var n = int.Parse(Console.ReadLine());
+            if (n == 1) {
+                amountPerStore = sales.GetPerStoreSales();
+            } else {
+                amountPerStore = sales.GetPerCategorySales();
+            }
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0},{1}", obj.Key, obj.Value);
+            }
         }
     }
 }
