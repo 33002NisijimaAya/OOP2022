6551867da3443e75ab693d50213a0d6754a932c0
33002西島彩 <33002@ojs.ac.jp>
2022-05-13 15:55:33 +0900

    問題2.3.2完成

--- Chapter03/Exercise01/Program.cs
+++ Chapter03/Exercise01/Program.cs
@@ -12,19 +12,23 @@ namespace Exercise01 {
             Exercise1_1(numbers);
             Console.WriteLine("--------------");
 
-            //Exercise1_2(numbers);
-            //Console.WriteLine("--------------");
+            Exercise1_2(numbers);
+            Console.WriteLine("--------------");
             
             
         }
 
         private static void Exercise1_1(List<int> numbers) {
             var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
-            Console.WriteLine(exists);
+            if (exists) {
+                Console.WriteLine("存在しています");
+            } else {
+                Console.WriteLine("存在していません");
+            }
         }
 
         private static void Exercise1_2(List<int> numbers) {
-            throw new NotImplementedException();
+            numbers.ForEach(s => Console.WriteLine(s/2.0));
         }
     }
 }
