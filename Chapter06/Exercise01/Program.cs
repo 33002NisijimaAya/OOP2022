e7cbf9437610038dd001664ad727b5ea38bfc383
33002西島彩 <33002@ojs.ac.jp>
2022-06-02 13:59:43 +0900

    問題6　全部完成

--- Chapter06/Exercise01/Program.cs
+++ Chapter06/Exercise01/Program.cs
@@ -30,19 +30,30 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(int[] numbers) {
-            
+            //最後の二つ要素取り出し
+            foreach (var n in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine(n);
+            }
         }
 
         private static void Exercise1_3(int[] numbers) {
-            
+            //数値を文字列に変換
+            var strs = numbers.Select(n => n.ToString());
+            foreach (var s in strs){
+                Console.WriteLine(s);
+            }
         }
 
         private static void Exercise1_4(int[] numbers) {
-            
+            var sortednumber = numbers.OrderBy(n => n).Where(n=>n>0).Take(3);
+            foreach (var num in sortednumber) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise1_5(int[] numbers) {
-            
+            var results = numbers.Distinct().Where(n=>n>10).Count();
+            Console.WriteLine(results);  
         }
     }
 }
