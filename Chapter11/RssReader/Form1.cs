ba460159025f135d61d63aa3400d762bcbb873ab
33002西島彩 <33002@ojs.ac.jp>
2022-06-28 15:57:22 +0900

    途中経過

---
+++ Chapter11/RssReader/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        IEnumerable<string> xTitle;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRssGet_Click(object sender, EventArgs e) {
+            
+            using (var wc = new WebClient()) {
+                var stream = wc.OpenRead(cbRssUrl.Text);
+                var xdoc = XDocument.Load(stream);
+                xTitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
+                
+                foreach (var data in xTitle) {
+                    lbRssTitle.Items.Add(data);
+                }
+                //wbBrowser = lbRssTitle.SelectedItems
+
+            }
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e) {
+            int index = lbRssTitle.SelectedIndex;   //選択した箇所のインデックスを取得（0～）
+            
+            
+        }
+
+        private void setcbCompany(string URL) {
+            if (!cbRssUrl.Items.Contains(URL)) {
+                //まだ登録されていなければ登録処理
+                cbRssUrl.Items.Add(URL);
+            }
+        }
+    }
+}
