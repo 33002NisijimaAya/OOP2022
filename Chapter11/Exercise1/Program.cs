73df667b97342c894f1ce69766bf6b16c4d862ea
33002西島彩 <33002@ojs.ac.jp>
2022-07-12 14:13:27 +0900

    12章まで終わり

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ using System.Xml.Linq;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
-            var file = "sample.xml";
+            var file = "Sample.xml";
             Exercise1_1(file);
             Console.WriteLine();
             Exercise1_2(file);
@@ -18,17 +19,20 @@ namespace Exercise1 {
 
             var newfile = "sports.xml";
             Exercise1_4(file, newfile);
+
+            var text = File.ReadAllText(newfile);
+            Console.WriteLine(text);
         }
 
         private static void Exercise1_1(string file) {
             var xdoc = XDocument.Load(file);
-            var xsportlists = xdoc.Root.Elements()
+            var sports = xdoc.Root.Elements()
                                     .Select(x => new {
                                         Name = (string)x.Element("name"),
                                         Member = x.Element("teammembers")
                                     });
-            foreach (var sportlist in xsportlists) { 
-                Console.WriteLine("{0} {1}", sportlist.Name,sportlist.Member.Value);
+            foreach (var sport in sports) {
+                Console.WriteLine("{0} {1}", sport.Name, sport.Member.Value);
             }
 
         }
@@ -37,26 +41,42 @@ namespace Exercise1 {
             var xdoc = XDocument.Load(file);
             var sportlists = xdoc.Root.Elements()
                                         .Select(x => new {
-                                            Name = (string)x.Element("name").Attribute("kanji"),
-                                            Year = (string)x.Element("firstplayed")
-                                        }).OrderBy(x=>x.Year);
+                                            Name = x.Element("name").Attribute("kanji").Value,
+                                            Year = x.Element("firstplayed").Value
+                                        }).OrderBy(x => x.Year);
             foreach (var sportlist in sportlists) {
-                Console.WriteLine("{0}({1})", sportlist.Name,sportlist.Year);
+                Console.WriteLine("{0}({1})", sportlist.Name, sportlist.Year);
             }
         }
 
         private static void Exercise1_3(string file) {
             var xdoc = XDocument.Load(file);
-            var maxmember = xdoc.Root.Elements()
-                                    .Select(x=> new {
-                                        Name = (string)x.Element("name"),
-                                        member = x.Element("teammembers")
-                                    }).Max();
-            Console.WriteLine(maxmember.Name);
+            var sport = xdoc.Root.Elements()
+                                    .Select(x => new {
+                                        Name = x.Element("name").Value,
+                                        member = x.Element("teammembers").Value
+                                    }).OrderByDescending(x => int.Parse(x.member)).First();
+
+            Console.WriteLine("{0} ({1}人)",sport.Name,sport.member);
+
         }
 
-        private static void Exercise1_4(string file, string newfile) {
 
+        private static void Exercise1_4(string file, string newfile) {
+            var xdoc = XDocument.Load(file);
+            var soccer = new XElement("file",
+                                new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                                new XElement("teammembers", "11"),
+                                new XElement("fistplayed", "1863")
+                                );
+            
+            xdoc.Root.Add(soccer);
+            xdoc.Save("sports.xml");
+            //foreach (var xsportlist in xdoc.Root.Elements()) {
+            //    var name = xsportlist.Element("name");
+            //    var member = xsportlist.Element("teammembers");
+            //    Console.WriteLine("{0} {1}", name.Value, member.Value);
+            //}
         }
     }
 }
