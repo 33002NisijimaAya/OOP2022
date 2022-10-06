dd7fd6cb6a7ab305b9c320211b861bc4624f9bc3
33002西島彩 <33002@ojs.ac.jp>
2022-10-06 14:45:44 +0900

    2まで完成

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -25,15 +25,37 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_2() {
-            
+            var book = Library.Books.First(b => b.Price == Library.Books.Max(x => x.Price));
+            Console.WriteLine(book);
         }
 
         private static void Exercise1_3() {
-              
+            var count1= 0;
+            var count2 = 0;
+            var count3 = 0;
+            var count4 = 0;
+            foreach (var book in Library.Books) {
+                if(book.PublishedYear == 2013) {
+                    count1++;
+                }
+                if (book.PublishedYear == 2014) {
+                    count2++;
+                }
+                if (book.PublishedYear == 2015) {
+                    count3++;
+                }
+                if (book.PublishedYear == 2016) {
+                    count4++;
+                }
+            }
+            Console.WriteLine($"2013年 {count1}冊");
+            Console.WriteLine($"2014年 {count2}冊");
+            Console.WriteLine($"2015年 {count3}冊");
+            Console.WriteLine($"2016年 {count4}冊");
         }
 
         private static void Exercise1_4() {
-
+            
         }
 
         private static void Exercise1_5() {
