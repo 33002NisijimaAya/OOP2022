e11fec22ee8724e735910ccc6f9c23e631979d90
33002西島彩 <33002@ojs.ac.jp>
2022-10-24 15:54:47 +0900

    枠線追加

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace CollarChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            
         }
+        
     }
 }
